7c85e2a92586cd4855993ac10d274e1c03d82414
Bao Toan <baotoanit1505@gmail.com>
2026-09-02 09:40:29 +0700

    first commit

---
+++ apag-ph-be/src/main/java/com/example/apag_ph_be/ApagPhBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.apag_ph_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApagPhBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApagPhBeApplication.class, args);
+	}
+
+}
---
+++ apag-ph-be/src/test/java/com/example/apag_ph_be/ApagPhBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apag_ph_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApagPhBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
